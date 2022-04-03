d0fd751941566f3e7db229a11d516c406730c5ed
Maxim Maxim <maximushtml@mail.ru>
2022-04-03 11:29:44 +0300

    Добавьте файлы проекта.

---
+++ TestProject/Container.h
@@ -0,0 +1,74 @@
+﻿#ifndef CONTAINER_H
+#define CONTAINER_H
+
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+
+// Функтор для вычисления значения хэш-функции std::pair<T, T>
+struct pair_hash {
+	unsigned hash_combine(unsigned lhs, unsigned rhs) const {
+		lhs ^= rhs + 0x9e3779b9 + (lhs << 6) + (lhs >> 2);
+		return lhs;
+	}
+	template <typename T>
+	unsigned operator()(const std::pair<T, T>& x) const {
+		return hash_combine(std::hash<T>()(x.first), std::hash<T>()(x.second));
+	}
+};
+
+// Вспомогательный класс для восстановления местоположения устройства
+template<typename T> struct DataWithIdx {
+	T data;													// element
+	const unsigned carId;									// element.carId
+	const unsigned deviceId;								// element.deviceId
+
+	DataWithIdx(T data_, std::pair<unsigned, unsigned> id) :
+		data(data_), carId(id.first), deviceId(id.second)
+	{}
+};
+
+template<typename T> class SubscriptHelper;
+
+template<typename T> class Container {
+	std::unordered_map<std::pair<unsigned, unsigned>, DataWithIdx<T>, pair_hash> data;
+
+public:
+	SubscriptHelper<T> operator[](unsigned carId) {
+		return SubscriptHelper<T>(this, carId);
+	}
+	DataWithIdx<T>& at(unsigned carId, unsigned deviceId) {
+		auto search = data.find(std::pair<unsigned, unsigned>(carId, deviceId));
+		if (search != data.end())
+			return search->second;
+		std::string exceptionMsg = "Non-existent carId = ";
+		exceptionMsg.append(std::to_string(carId));
+		exceptionMsg.append(" & deviceId = ");
+		exceptionMsg.append(std::to_string(deviceId));
+		throw std::out_of_range(exceptionMsg);
+	}
+	bool insert(T in, unsigned carId, unsigned deviceId) {
+		auto res = data.insert({ std::pair<unsigned, unsigned>(carId, deviceId),
+			DataWithIdx<T>(in,std::pair<unsigned, unsigned>(carId, deviceId)) });
+		return res.second;
+	}
+};
+
+// Вспомогательный класс для реализации обращения к элементам
+// в требуемом виде container[carId][deviceId]
+template<typename T> class SubscriptHelper
+{
+	Container<T>* container;
+	int carId;
+
+public:
+	DataWithIdx<T>& operator[](unsigned deviceId) {
+		return container->at(carId, deviceId);
+	}
+
+private:
+	friend Container<T>;
+	SubscriptHelper(Container<T>* main, int carId_) : container(main), carId(carId_) {}
+};
+
+#endif
---
+++ TestProject/ContainerTests.cpp
@@ -0,0 +1,98 @@
+﻿#include "Container.h"
+#include <cassert>
+#include <iostream>
+#include <stdexcept>
+
+struct TestStruct1 { int x{}; int y{}; double z{}; };
+struct TestStruct2 { std::string text; int a{}; };
+
+int main()
+{
+	try {
+		Container<TestStruct1> container1;
+		
+		TestStruct1 ins1{ 10,20,30.0 };
+		TestStruct1 ins2{ 100,200,300.0 };
+		bool res1 = container1.insert(ins1, 1, 2); // OK
+		assert(res1 == true);
+		bool res2 = container1.insert(ins2, 1, 2); // Fail - индекс уже существует
+		assert(res2 == false);
+		bool res3 = container1.insert(ins2, 2, 1); // OK
+		assert(res3 == true);
+		
+		// Получение данных по ссылке
+		auto& ref1 = container1[1][2];
+		// Проверка на равенство добавленных и извлечённых данных
+		assert(
+			ref1.carId  == 1 &&
+			ref1.deviceId == 2 &&
+			ref1.data.x == ins1.x &&	// 10   == 10
+			ref1.data.y == ins1.y &&	// 20   == 20
+			ref1.data.z == ins1.z);		// 30.0 == 30.0
+
+		ref1.data.x = 1;
+		ref1.data.y = 2;
+		// Проверка на частичное равенство добавленных и изменённых данных
+		assert(
+			ref1.data.x != ins1.x &&	//  1   != 10
+			ref1.data.y != ins1.y &&	//  2   != 20
+			ref1.data.z == ins1.z);		// 30.0 == 30.0
+
+		// Получение копии данных
+		auto copy1 = container1[2][1];
+		copy1.data.x = 1000;
+		auto& ref2 = container1[2][1];
+		assert(
+			copy1.carId    == ref2.carId &&
+			copy1.deviceId == ref2.deviceId &&
+			copy1.data.x   != ref2.data.x);
+		
+
+		Container<TestStruct2> container2;
+		TestStruct2 ins3{ "Hello!", 123 };
+		TestStruct2 ins4{ "vniias", 6 };
+
+		bool res4 = container2.insert(ins3, 4, 3);
+		assert(res4 == true);
+		bool res5 = container2.insert(ins4, 4, 3);
+		assert(res5 == false);
+		bool res6 = container2.insert(ins4, 404, 300);
+		assert(res6 == true);
+		
+		// Получение данных по ссылке
+		auto& ref3 = container2[4][3];
+		// Проверка на равенство добавленных и извлечённых данных
+		assert(
+			ref3.carId == 4 &&
+			ref3.deviceId == 3 &&
+			ref3.data.text == ins3.text &&	// Hello!
+			ref3.data.a == ins3.a);			// 123 == 123
+
+		ref3.data.text = "Good bye!";
+		ref3.data.a = 321;
+		//ref3.carId = 0;					// Ошибка компиляции
+		assert(
+			ref3.carId == 4 &&
+			ref3.deviceId == 3 &&
+			ref3.data.text != ins3.text &&	// Good bye! != Hello!
+			ref3.data.a != ins3.a);			// 321 != 123
+
+		auto& ref4 = container2[404][300];
+		assert(
+			ref4.carId == 404 &&
+			ref4.deviceId == 300 &&
+			ref4.data.text == ins4.text &&	// vniias
+			ref4.data.a == ins4.a);			// 6 == 6
+
+		//SubscriptHelper<TestStruct2>(&container2, 100); // Ошибка компиляции
+
+		container2[3][4];	// Выброс исключения
+	}
+	catch (std::out_of_range& exception) {
+		std::cout << exception.what() << std::endl;
+	}
+	catch (...) {
+		std::cout << "Unknown error\n";
+	}
+	return 0;
+}
